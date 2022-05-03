b139e9f42a90949e3cba68822bf9f06a887f9bd0
neevek <i@neevek.net>
2022-05-03 22:46:09 +0800

    improve telescope layout

--- highlights.lua
+++ highlights.lua
@@ -7,9 +7,10 @@ local bg = require("core.utils").bg
 
 fg("TelescopeBorder", colors.yellow, colors.black2)
 fg("TelescopePromptBorder", colors.yellow, colors.black2)
+fg("TelescopePromptCounter", colors.yellow)
 fg_bg("TelescopePreviewTitle", colors.orange, colors.black2)
 fg_bg("TelescopePromptTitle", colors.orange, colors.black2)
-fg_bg("TelescopeResultsTitle", colors.green, colors.black2)
+fg_bg("TelescopeResultsTitle", colors.orange, colors.black2)
 fg_bg("TelescopeSelection", colors.yellow, colors.grey)
 
 fg("AlphaHeader", colors.nord_blue)
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,11 +1,16 @@
 local M = {
+  defaults = {
+    layout_config = {
+      width = 0.9,
+    },
+  },
   extensions = {
     ["ui-select"] = {
       require("telescope.themes").get_cursor {
         -- more opts
       },
     },
-    ["fzf"] = {
+    fzf = {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
